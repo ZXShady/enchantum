4c47450dbe27833cb13e11a28d3a19e0816f0afb
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-04-24 10:00:40 +0200

    Add `all.hpp` header
    It includes all the stuff including formatting

---
+++ enchantum/include/enchantum/all.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "enchantum.hpp"
+#include "algorithms.hpp"
+#include "cast.hpp"
+#include "next_value.hpp"
+#if __has_include(<fmt/format.h>)
+#include "fmt_format.hpp"
+#else
+#include "std_format.hpp"
+#endif
