827f00871e90e2f3744121ab5b5cc8a04d51cffa
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-04-30 00:57:13 +0200

    Remove benchmark main and update script

--- benchmarks/generate_compile_time_tests.py
+++ benchmarks/generate_compile_time_tests.py
@@ -1,6 +1,8 @@
 ﻿# Get user-defined input
 start = int(input("Enter start of range (inclusive): "))
+
 end = int(input("Enter end of range (exclusive): "))
+is_enum_class = bool(input("Enum classes? (True/False)"))
 value_count = int(input("Enter number of enum values per enum (e.g., 15): "))
 base_filename = input("Enter base file name (e.g., big_enums): ").strip()
 max_range = int(input("Enter the max range for MAGIC_ENUM_RANGE_MAX and ENCHANTUM_MAX_RANGE: "))
@@ -21,7 +23,7 @@ def create_test(magic_enum: bool, filename: str):
 
         for i in range(start, end):
             enum_name = f"A_{i-start}"
-            f.write(f"enum class {enum_name} : int {{\n")
+            f.write(f"enum {"class" if is_enum_class else "" } {enum_name} : int {{\n")
             values = [f"  {enum_name}_{j} = {j}" for j in range(value_count)]
             f.write(",\n".join(values))
             f.write("\n};\n\n")
@@ -34,13 +36,9 @@ def create_test(magic_enum: bool, filename: str):
                     f.write(f'static_assert(magic_enum::enum_name({enum_name}::{value_name}) == "{value_name}");\n')
                 else:
                     f.write(f'static_assert(enchantum::to_string({enum_name}::{value_name}) == "{value_name}");\n')
+    print(f"File '{filename}' generated successfully with enums from {start} to {end - 1}.")
 
 # Generate both tests
-magic_enum_file = f"magic_enum_{base_filename}.cpp"
-enchantum_file = f"enchantum_{base_filename}.cpp"
-
-create_test(True, magic_enum_file)
-print(f"File '{magic_enum_file}' generated successfully with enums from {start} to {end - 1}.")
-
-create_test(False, enchantum_file)
-print(f"File '{enchantum_file}' generated successfully with enums from {start} to {end - 1}.")
+create_test(True, f"magic_enum_{base_filename}.cpp")
+create_test(False, f"enchantum_{base_filename}.cpp")
+input("Press enter to exit...")
