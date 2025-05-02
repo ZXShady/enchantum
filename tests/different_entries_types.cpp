981e15fa280ed20837db15337651861ec50b489d
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-05-02 15:29:29 +0200

    Run `different_entries_types` test
    The reason I am using INFO and stuff is because I am experiencign linker errors if I use `CHECK` with an expression containing a `std::string_view`???

--- tests/different_entries_types.cpp
+++ tests/different_entries_types.cpp
@@ -1,6 +1,7 @@
 #include "test_utility.hpp"
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_tostring.hpp>
 #include <enchantum/enchantum.hpp>
 
 template<typename E>
@@ -8,11 +9,17 @@ struct ValueAndString {
   E                value;
   std::string_view string;
 };
-#if 0
+
 TEMPLATE_LIST_TEST_CASE("KV as entries key-value pair", "[entries][override_key_value_pair]", AllEnumsTestTypes)
 {
   for (const auto& kv : enchantum::entries<TestType, ValueAndString<TestType>>) {
-    CHECK(kv.string == enchantum::to_string(kv.value));
+    if (kv.string != enchantum::to_string(kv.value)) {
+      INFO(R"(CHECK(string == enchantum::to_string(value))
+with expansion:
+)");
+
+      INFO('"' << kv.string << '"' << " != " << '"' << enchantum::to_string(kv.value) << '"');
+      CHECK(false);
+    }
   }
 }
-#endif
