9f79fc1fb9720553a3ee4045058094d78ec6d7e5
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-05-07 13:49:34 +0200

    Add single include header (not tested)
    Generated using `qoum`
    
    `quom all.hpp single_header.hpp`
    
    Primarily for godbolt links

--- enchantum/include/enchantum/all.hpp
+++ enchantum/include/enchantum/all.hpp
@@ -3,6 +3,7 @@
 #include "enchantum.hpp"
 #include "algorithms.hpp"
 #include "next_value.hpp"
+#include "array.hpp"
 #if __has_include(<fmt/format.h>)
 #include "fmt_format.hpp"
 #elif __has_include(<format>)
@@ -11,3 +12,4 @@
 #include "bitflags.hpp"
 #include "iostream.hpp"
 #include "bitwise_operators.hpp"
+#include "entries.hpp"
