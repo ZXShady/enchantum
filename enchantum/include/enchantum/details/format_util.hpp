4dccb1f975069fbd27cbcb3fff4b1e2aecfb3745
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-06-03 12:40:44 +0200

    Add missing `#pragma once`

--- enchantum/include/enchantum/details/format_util.hpp
+++ enchantum/include/enchantum/details/format_util.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "../enchantum.hpp"
 #include <string>
 
---
+++ tests/double_include.cpp
@@ -0,0 +1,35 @@
+#include <enchantum/algorithms.hpp>
+#include <enchantum/array.hpp>
+#include <enchantum/bitflags.hpp>
+#include <enchantum/bitset.hpp>
+#include <enchantum/bitwise_operators.hpp>
+#include <enchantum/details/generate_arrays.hpp>
+#include <enchantum/details/optional.hpp>
+#include <enchantum/details/string.hpp>
+#include <enchantum/details/string_view.hpp>
+#include <enchantum/enchantum.hpp>
+#include <enchantum/entries.hpp>
+#include <enchantum/iostream.hpp>
+#include <enchantum/next_value.hpp>
+
+#if __has_include(<format>)
+  #include <enchantum/std_format.hpp>
+#endif
+
+#include <enchantum/algorithms.hpp>
+#include <enchantum/array.hpp>
+#include <enchantum/bitflags.hpp>
+#include <enchantum/bitset.hpp>
+#include <enchantum/bitwise_operators.hpp>
+#include <enchantum/details/generate_arrays.hpp>
+#include <enchantum/details/optional.hpp>
+#include <enchantum/details/string.hpp>
+#include <enchantum/details/string_view.hpp>
+#include <enchantum/enchantum.hpp>
+#include <enchantum/entries.hpp>
+#include <enchantum/iostream.hpp>
+#include <enchantum/next_value.hpp>
+
+#if __has_include(<format>)
+  #include <enchantum/std_format.hpp>
+#endif
