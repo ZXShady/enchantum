730ebfcc6b8a33f6517513c09805bdfcf644472f
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-07-07 23:01:56 +0200

    Remove unnecessary using declarations

--- enchantum/include/enchantum/array.hpp
+++ enchantum/include/enchantum/array.hpp
@@ -11,17 +11,8 @@ private:
   using base = std::array<V, count<E>>;
 public:
   using index_type = E;
-  using typename base::const_iterator;
-  using typename base::const_pointer;
-  using typename base::const_reference;
-  using typename base::const_reverse_iterator;
-  using typename base::difference_type;
-  using typename base::iterator;
-  using typename base::pointer;
   using typename base::reference;
-  using typename base::reverse_iterator;
-  using typename base::size_type;
-  using typename base::value_type;
+  using typename base::const_reference;
 
   using base::at;
   using base::operator[];
